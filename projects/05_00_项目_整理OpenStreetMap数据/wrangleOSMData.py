279bc84481eddfa1e24808e49b72d8d143fde11c
yjwang1314 <ajune_6@hotmail.com>
2018-02-19 16:42:14 +0800

    update wrangle data code
    to count tags, audit, look of tag problem

--- projects/05_00_项目_整理OpenStreetMap数据/wrangleOSMData.py
+++ projects/05_00_项目_整理OpenStreetMap数据/wrangleOSMData.py
@@ -7,6 +7,11 @@ Created on Mon Feb 12 17:39:42 2018
 
 import xml.etree.cElementTree as ET
 import pprint
+import re
+
+lower = re.compile(r'^([a-z]|_)*$') # 表示仅包含小写字母且有效的标记
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$') # 表示名称中有冒号的其他有效标记
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]') # 表示字符存在问题的标记
 
 def count_tags(filename):
     tags_list = []
@@ -16,10 +21,41 @@ def count_tags(filename):
     tags = dict(map(lambda x: [x, tags_list.count(x)], tags))
     return tags
 
-def test():
+def count_tags_test():
 
-    tags = count_tags('map.osm')
+    tags = count_tags('map_sample.osm')
     pprint.pprint(tags)    
+    
+
+def key_type(element, keys):
+    if element.tag == "tag":
+        # YOUR CODE HERE
+        k = element.attrib['k']
+        if len(re.findall(lower, k)):
+            keys['lower'] += 1
+        elif len(re.findall(lower_colon, k)):
+            keys['lower_colon'] += 1
+            # print k
+        elif len(re.findall(problemchars, k)):
+            keys['problemchars'] += 1
+        else:
+            keys['other'] += 1
+            print k
+        
+    return keys
+
+def process_map(filename):
+    keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
+    for _, element in ET.iterparse(filename):
+        keys = key_type(element, keys)
+
+    return keys
+
+
+def test():
+    keys = process_map('map_sample.osm')
+    pprint.pprint(keys)
+    # assert keys == {'lower': 5, 'lower_colon': 0, 'other': 1, 'problemchars': 1}
 
 if __name__ == "__main__":
     test()
