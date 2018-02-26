63f8547f828a2932c0e216dd7ec94a88b6101a4f
yjwang1314 <ajune_6@hotmail.com>
2018-02-26 16:16:16 +0800

    create restaurant fastfood bank name updates

--- projects/05_00_项目_整理OpenStreetMap数据/data.py
+++ projects/05_00_项目_整理OpenStreetMap数据/data.py
@@ -18,6 +18,18 @@ lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
 CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
+
+changes = {u'中国农业银行': u'农业银行',
+           u'中国工商银行': u'工商银行',
+           u'中国建设银行': u'建设银行',
+           u'中国民生银行': u'民生银行',
+           "McDonald's": u'麦当劳',
+           'McDonald': u'麦当劳',
+           u"金拱门 麥當勞 Mc'Donald": u'麦当劳',
+           'Pizza Hut': u'必胜客',
+           'KFC': u'肯德基',
+           'Subway': u'赛百味'} 
+
 # UPDATE THIS VARIABLE
 mapping = { "St": "Street",
             "St.": "Street",
@@ -78,6 +90,10 @@ def shape_element(element):
                                 address[addr_k[0]] = better_v
                                 node['address'] = address
                         elif k.find(':') == -1 and k == 'name':
+                            if v in changes.keys():
+                                v = changes[v]
+                            else:
+                                pass
                             node[k] = update_name(v, mapping)
                         elif k.find(':') == -1:
                             node[k] = v
@@ -106,7 +122,7 @@ def process_map(file_in, pretty = False):
     return data
 
 def test():
-    data = process_map('map.osm', True)
+    data = process_map('map_sample.osm', True)
     #pprint.pprint(data)
 
 if __name__ == "__main__":
