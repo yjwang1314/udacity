44216070f5d6953897066002cdc57f01c46404d0
yjwang1314 <ajune_6@hotmail.com>
2018-02-28 14:51:22 +0800

    practice naive bayse mini project

--- projects/09_00_项目_机器学习/ud120-projects-master/naive_bayes/nb_author_id.py
+++ projects/09_00_项目_机器学习/ud120-projects-master/naive_bayes/nb_author_id.py
@@ -9,7 +9,8 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
+from sklearn.naive_bayes import GaussianNB    
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,13 +22,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+# training time: 1.341 s
+                             
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+# predict time: 0.234 s
 #########################################################
 
-
+accuracy = clf.score(features_test, labels_test)
+print "predict accuracy:", round(accuracy, 4)
