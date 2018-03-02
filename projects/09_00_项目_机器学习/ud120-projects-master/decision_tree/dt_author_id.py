c68d0b167451b7e0c29c0ce4f86b0138ad51cad2
yjwang1314 <ajune_6@hotmail.com>
2018-03-02 14:31:47 +0800

    decision tree mini project

--- projects/09_00_项目_机器学习/ud120-projects-master/decision_tree/dt_author_id.py
+++ projects/09_00_项目_机器学习/ud120-projects-master/decision_tree/dt_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
 
 
 ### features_train and features_test are the features for the training
@@ -20,12 +21,26 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+# training time:  98.976 s
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+# predict time: 0.075 s
+
+acc = clf.score(features_test, labels_test)
+print 'accuracy:', round(acc, 4)
+# accuracy: 0.9767
 
 #########################################################
 
-
+# 通过特征选择加速
+len(features_train[0]) # 特征数 3785
+# 1% 特征下的准确率为0.9664，但时间减少了很多
