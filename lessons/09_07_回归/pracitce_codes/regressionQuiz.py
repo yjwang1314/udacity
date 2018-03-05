1f63100d0c03c638dbc860b0b4bcdf022f32f65e
yjwang1314 <ajune_6@hotmail.com>
2018-03-05 16:48:19 +0800

    update quiz codes

--- lessons/09_07_回归/pracitce_codes/regressionQuiz.py
+++ lessons/09_07_回归/pracitce_codes/regressionQuiz.py
@@ -26,24 +26,24 @@ reg.fit(ages_train, net_worths_train)
 ### argument to your prediction function is in the expected format - if you get
 ### a warning about needing a 2d array for your data, a list of lists will be
 ### interpreted by sklearn as such (e.g. [[27]]).
-km_net_worth = 1.0 ### fill in the line of code to get the right value
+km_net_worth = reg.predict(27)[0][0]
 
 ### get the slope
 ### again, you'll get a 2-D array, so stick the [0][0] at the end
-slope = 0. ### fill in the line of code to get the right value
+slope = reg.coef_[0][0]
 
 ### get the intercept
 ### here you get a 1-D array, so stick [0] on the end to access
 ### the info we want
-intercept = 0. ### fill in the line of code to get the right value
+intercept = reg.intercept_[0]
 
 
 ### get the score on test data
-test_score = 0. ### fill in the line of code to get the right value
+test_score = reg.score(ages_test, net_worths_test)
 
 
 ### get the score on the training data
-training_score = 0. ### fill in the line of code to get the right value
+training_score = reg.score(ages_train, net_worths_train)
 
 
 
