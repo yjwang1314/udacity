6729c8893b75c91bc77a38649330f46beb17635d
yjwang1314 <ajune_6@hotmail.com>
2018-03-05 17:19:57 +0800

    regression mini project

--- projects/09_00_项目_机器学习/ud120-projects-master/regression/finance_regression.py
+++ projects/09_00_项目_机器学习/ud120-projects-master/regression/finance_regression.py
@@ -29,20 +29,16 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
+test_color = "r"
 
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -56,15 +52,72 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
+### draw the regression line, once it's coded
+try:
+    plt.plot( feature_test, reg.predict(feature_test) )
+except NameError:
+    pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
+plt.xlabel(features_list[1])
+plt.ylabel(features_list[0])
+plt.legend()
+plt.show()
+
+##################################################
+slope = reg.coef_[0]
+intercep = reg.intercept_
+r2_train = reg.score(feature_train, target_train) # 0.0455
+r2_test = reg.score(feature_test, target_test) # -1.48499
+
+                 
+##################################################
+features_list = ["bonus", "long_term_incentive"]
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+target, features = targetFeatureSplit( data )
+
+### training-testing split needed in regression, just like classification
+from sklearn.cross_validation import train_test_split
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+train_color = "b"
+test_color = "r"
 
 
+### Your regression goes here!
+### Please name it reg, so that the plotting code below picks it up and 
+### plots it correctly. Don't forget to change the test_color above from "b" to
+### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+
+
+### draw the scatterplot, with color-coded training and testing points
+import matplotlib.pyplot as plt
+for feature, target in zip(feature_test, target_test):
+    plt.scatter( feature, target, color=test_color ) 
+for feature, target in zip(feature_train, target_train):
+    plt.scatter( feature, target, color=train_color ) 
+
+### labels for the legend
+plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
+plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+##################################################
+slope = reg.coef_[0]
+intercep = reg.intercept_
+r2_train = reg.score(feature_train, target_train) # 0.2170
+r2_test = reg.score(feature_test, target_test) # -0.5927                 
