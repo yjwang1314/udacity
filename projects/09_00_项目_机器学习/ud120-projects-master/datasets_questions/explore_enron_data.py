f77dd23dc412361c51f561a8207a3ae9674d53fe
yjwang1314 <ajune_6@hotmail.com>
2018-03-05 15:08:30 +0800

    practice explore enron data

--- projects/09_00_项目_机器学习/ud120-projects-master/datasets_questions/explore_enron_data.py
+++ projects/09_00_项目_机器学习/ud120-projects-master/datasets_questions/explore_enron_data.py
@@ -19,4 +19,21 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+nb_poi = sum(map(lambda x: x['poi'], enron_data.values())) # 嫌疑人数量
+enron_data['PRENTICE JAMES']['total_stock_value']
+enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+enron_data['SKILLING JEFFREY K']['exercised_stock_options']
 
+enron_data['SKILLING JEFFREY K']['total_payments'] # CEO
+enron_data['LAY KENNETH L']['total_payments'] # Founder
+enron_data['FASTOW ANDREW S']['total_payments'] # CFO
+
+nb_sal = sum(map(lambda x: x['salary'] != 'NaN', enron_data.values())) # salary数量          
+nb_email = sum(map(lambda x: x['email_address'] != 'NaN', enron_data.values())) # email数量         
+
+nb_pay = sum(map(lambda x: x['total_payments'] == 'NaN', enron_data.values()))
+float(nb_pay) / len(enron_data)
+
+nb_pay = sum(map(lambda x: x['total_payments'] == 'NaN' and x['poi'], enron_data.values()))
+
+nb_stk = sum(map(lambda x: x['total_stock_value'] == 'NaN' and x['poi'], enron_data.values()))
