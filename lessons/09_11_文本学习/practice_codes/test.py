704b18a13f158acdfe6012a78ce3564b7da29bfc
yjwang1314 <ajune_6@hotmail.com>
2018-03-07 15:18:31 +0800

    text learning practice codes

---
+++ lessons/09_11_文本学习/practice_codes/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 07 14:40:19 2018
+
+@author: Jun Wang
+"""
+
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download()
+sw = stopwords.words('english')
