499622c48367e1096fc202a788dbd83e065c4dce
yjwang1314 <ajune_6@hotmail.com>
2018-03-09 17:45:54 +0800

    final project remove outliers, feature selecting

--- projects/09_00_项目_机器学习/ud120-projects-master/final_project/poi_id.py
+++ projects/09_00_项目_机器学习/ud120-projects-master/final_project/poi_id.py
@@ -10,20 +10,44 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 
+                 'loan_advances', 'bonus', 'deferred_income', 'restricted_stock_deferred', 
+                 'total_stock_value', 'expenses', 'exercised_stock_options', 
+                 'long_term_incentive', 'restricted_stock', 'director_fees',
+                 'to_messages', 'from_poi_to_this_person', 'from_messages', 
+                 'from_this_person_to_poi', 'shared_receipt_with_poi'] 
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+
+# from findOutliers import findOutlier_NaN, findOutlier_visual
+## 2.1 find outliers by outliers data 'NaN' and remove
+# key_nan = findOutlier_NaN(data_dict)
+
+## 2.2 find outliers by visualization and remove
+# key = findOutlier_visual(data_dict)
+
+## 2.3 remove
+data_dict.pop('LOCKHART EUGENE E') # No data available on this person.
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK') # Not a person/employee associated with Enron
+data_dict.pop('TOTAL') # Summation of everyone's data
+
 ### Task 3: Create new feature(s)
+
+## 3.1 select kbest features
+from feature_selecting import Select_K_Best
+k_best_features = Select_K_Best(data_dict, features_list, 15)
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -53,3 +77,8 @@ features_train, features_test, labels_train, labels_test = \
 ### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
+
+'''
+https://github.com/nehal96/Machine-Learning-Enron-Fraud
+https://github.com/yijigao/Enron_project
+'''
