3d6df0d77e375225e27109240907992e69d09a05
yjwang1314 <ajune_6@hotmail.com>
2018-03-09 10:32:10 +0800

    validate mini project

--- projects/09_00_项目_机器学习/ud120-projects-master/validation/validate_poi.py
+++ projects/09_00_项目_机器学习/ud120-projects-master/validation/validate_poi.py
@@ -28,5 +28,26 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+
+# split train data and test data, and train, test
+features_train, features_test, labels_train, labels_test = train_test_split(
+        features, labels, test_size=0.3, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+accuracy = clf.score(features_test, labels_test)
+print 'accuracy:', accuracy # 0.7916666
 
 
+# search for best parameter
+from sklearn.grid_search import GridSearchCV
+
+param_grid = {'min_samples_split': [2, 5, 10, 30],
+              'max_depth': [None, 2, 5, 10]}
+
+clf = GridSearchCV(DecisionTreeClassifier(), param_grid)
+clf = clf.fit(features_train, labels_train)
+accuracy_best = clf.score(features_test, labels_test)
+print 'accuracy_best:', accuracy_best # 0.8620689
+best_param = clf.best_params_ # best parameter
