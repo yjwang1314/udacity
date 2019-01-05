d368de225f91ecaee39d761058ff418a467928e3
jun_surface4 <yjwang1314@163.com>
2019-01-05 17:20:39 +0800

    linear regression practise notes

---
+++ DS/lessons/supervised learning/1-linear regression/lasso_regression_quiz_4.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 05 17:05:24 2019
+
+@author: Yijun Wang
+"""
+
+# TODO: Add import statements
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = pd.read_csv('quiz_4_data.csv', header = None)
+X = train_data.iloc[:,:-1]
+y = train_data.iloc[:,-1]
+
+# TODO: Create the linear regression model with lasso regularization.
+lasso_reg = Lasso()
+
+# TODO: Fit the model.
+lasso_reg.fit(X, y)
+
+# TODO: Retrieve and print out the coefficients from the regression model.
+reg_coef = lasso_reg.coef_
+print(reg_coef)
