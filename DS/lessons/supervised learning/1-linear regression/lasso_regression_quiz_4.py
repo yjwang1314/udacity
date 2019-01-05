82b57fb58bc53e5a6b2f861902162f0676dfe7e3
jun_surface4 <yjwang1314@163.com>
2019-01-05 18:22:29 +0800

    perceptron algorithm practise notes

--- DS/lessons/supervised learning/1-linear regression/lasso_regression_quiz_4.py
+++ DS/lessons/supervised learning/1-linear regression/lasso_regression_quiz_4.py
@@ -9,12 +9,19 @@ Created on Sat Jan 05 17:05:24 2019
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
 
 # Assign the data to predictor and outcome variables
 # TODO: Load the data
 train_data = pd.read_csv('quiz_4_data.csv', header = None)
 X = train_data.iloc[:,:-1]
 y = train_data.iloc[:,-1]
+
+# TODO: Create the standardization scaling object.
+scaler = StandardScaler()
+
+# TODO: Fit the standardization parameters and scale the data.
+X_scaled = scaler.fit_transform(X)
 
 # TODO: Create the linear regression model with lasso regularization.
 lasso_reg = Lasso()
